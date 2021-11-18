7592527a9445f6ad05406b7cdeadf86b62629a1c
SJ Gohil <94597717+biz-sjgohil@users.noreply.github.com>
2021-11-18 18:08:27 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,12 @@
-<?php
-phpinfo();
+<html><head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>First Website</title>
+</head>
+<body data-new-gr-c-s-check-loaded="8.890.0" data-gr-ext-installed="">
+	<div>
+		PHP working
+	</div>
+
+</body>
+</html>
